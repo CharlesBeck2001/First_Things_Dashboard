eaaffa43f9d695ed5542dca21952154a47262988
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-04-02 18:08:28 -0400

    Update Dashboard_Code.py

--- Dashboard_Code.py
+++ Dashboard_Code.py
@@ -221,7 +221,7 @@ if st.session_state.authenticated:
                 f"""
                 <div style="border: 2px solid white; padding: 20px; border-radius: 10px; background-color: black; color: white;">
                     <h3 style="font-size: 16px;">Total Customers</h3>
-                    <h4 style="font-size: 24px;">{total_customers:,f}</h4>
+                    <h4 style="font-size: 24px;">{total_customers:,}</h4>
                 </div>
                 """, unsafe_allow_html=True)
     
